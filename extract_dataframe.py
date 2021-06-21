17c6e6d368f1dd1bae45eb436f40962822953547
damukuzi <damukuzi@gmail.com>
2021-06-21 22:46:40 +0300

    finishing up extract dataframe

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -44,6 +44,13 @@ class TweetDfExtractor:
 
         return text
 
+    def find_sentiments(self)->list:
+        text = [x['text'] for x in self.tweets_list]
+        subjectivity= [TextBlob(word).sentiment.subjectivity for word in text]
+        polarity = [TextBlob(word).sentiment.polarity for word in text]   
+        return polarity,subjectivity
+
+
     def find_created_time(self)->list:
         created_at = [x['created_at'] for x in self.tweets_list]
 
@@ -114,34 +121,31 @@ class TweetDfExtractor:
         
         return lang
 
-    
         
-        
-    def get_tweet_df(self, save=False)->pd.DataFrame:
+    def get_tweet_df(self, save=True)->pd.DataFrame:
         """required column to be generated you should be creative and add more features"""
         
-        columns = ['created_at', 'source', 'original_text', 'lang', 'favorite_count', 'retweet_count', 
-            'original_author', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place']
+        columns = ['created_at', 'source', 'original_text','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
+            'original_author', 'followers_count','friends_count', 'hashtags', 'user_mentions', 'place']
         
         created_at = self.find_created_time()
         source = self.find_source()
         text = self.find_full_text()
-        #polarity, subjectivity = self.find_sentiments(text)
+        polarity, subjectivity = self.find_sentiments()
         lang = self.find_lang()
         fav_count = self.find_favourite_count()
         retweet_count = self.find_retweet_count()
         screen_name = self.find_screen_name()
         follower_count = self.find_followers_count()
         friends_count = self.find_friends_count()
-        sensitivity = self.is_sensitive()
         hashtags = self.find_hashtags()
         mentions = self.find_mentions()
         location = self.find_location()
-        data = zip(created_at, source, text, fav_count, retweet_count, screen_name, follower_count, friends_count, sensitivity, hashtags, mentions, location)
+        data = zip(created_at, source, text, polarity, subjectivity,lang, fav_count, retweet_count, screen_name, follower_count, friends_count,hashtags, mentions, location)
         df = pd.DataFrame(data=data, columns=columns)
 
         if save:
-            df.to_csv('processed_tweet_data.csv', index=False)
+            df.to_csv('data/processed_tweet_data.csv', index=False)
             print('File Successfully Saved.!!!')
         
         return df
